5a0631a590ffa490acf5325be67482dad7879791
niyf <niyf@pukkasoft.cn>
2018-03-06 10:34:45 +0800

    drag

---
+++ index.js
@@ -0,0 +1,17 @@
+function allowDrop(ev)
+{
+ev.preventDefault();
+}
+
+function drag(ev)
+{
+ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev)
+{
+ev.preventDefault();
+var data=ev.dataTransfer.getData("Text");
+console.log(ev)
+ev.target.appendChild(document.getElementById(data));
+}
