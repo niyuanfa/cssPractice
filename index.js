d343290ef9e9333ea6203bd3ad125e48e9b94f3c
niyf <niyf@pukkasoft.cn>
2018-03-06 17:21:04 +0800

    drag

--- index.js
+++ index.js
@@ -15,3 +15,46 @@ var data=ev.dataTransfer.getData("Text");
 console.log(ev)
 ev.target.appendChild(document.getElementById(data));
 }
+
+var dragEl = null;
+function domdrugstart(e) {
+    e.target.style.opacity = '0.5';
+    dragEl = e.target;
+    console.log(dragEl)
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html",e.target.innerHTML);
+  }
+  function domdrugenter(e) {
+    e.target.classList.add('over');
+  }
+  function domdrugover(e) {
+    if (e.preventDefault) {
+      e.preventDefault(); 
+    }
+
+    e.dataTransfer.dropEffect = 'move';
+
+    return false;
+  }
+  function domdrugleave(e) {
+    e.target.classList.remove('over'); 
+  }   
+  function domdrop(e) {
+    console.log("第五步",e)
+    if (e.stopPropagation) {
+      e.stopPropagation();
+    }
+    console.log(dragEl)
+    if (dragEl != e.target) {
+    console.log("ok")
+      dragEl.innerHTML = e.target.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }    
+    
+    return false;
+  }
+  function domdrapend(e) {
+    console.log("第六步")
+      e.target.classList.remove('over');
+      e.target.style.opacity = '1';
+  }     
